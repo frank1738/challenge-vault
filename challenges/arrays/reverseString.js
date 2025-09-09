e5b4db130e8791b5ad877aa7d9f70d27952ee531
frank1738 <franklineosoro08@gmail.com>
2025-09-09 22:07:32 +0300

    Optimize reverse string

--- challenges/arrays/reverseString.js
+++ challenges/arrays/reverseString.js
@@ -1,14 +1,11 @@
 // Write a function that reverses a string. The input string is given as an array of characters s.
 
 const reverseString = (s) => {
+  let leftPointer = 0;
   let rightPointer = s.length - 1;
-  for (let i = 0; i < s.length; i++) {
-    if (i >= rightPointer) return s;
-    const temp = s[i];
-    s[i] = s[rightPointer];
-    s[rightPointer] = temp;
+  while (leftPointer < rightPointer) {
+    [s[leftPointer], s[rightPointer]] = [s[rightPointer], s[leftPointer]];
+    leftPointer++;
     rightPointer--;
   }
 };
-
-console.log(reverseString(['a', 'b', 'c', 'd', 'e']));
