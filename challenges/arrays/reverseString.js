7c94c27a1a86658d986a097a3e8b08d960274290
frank1738 <franklineosoro08@gmail.com>
2025-09-09 19:51:34 +0300

    Reverse a string

---
+++ challenges/arrays/reverseString.js
@@ -0,0 +1,14 @@
+// Write a function that reverses a string. The input string is given as an array of characters s.
+
+const reversestring = (s) => {
+  let rightPointer = s.length - 1;
+  for (let i = 0; i < s.length; i++) {
+    if (i >= rightPointer) return s;
+    const temp = s[i];
+    s[i] = s[rightPointer];
+    s[rightPointer] = temp;
+    rightPointer--;
+  }
+};
+
+console.log(reversestring(['a', 'b', 'c', 'd', 'e']));
